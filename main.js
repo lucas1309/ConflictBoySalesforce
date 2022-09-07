53c3bfc9d742e36610fae1964e22c02d1f7abfeb
DESKTOP-MVFG55O\lucas <lucas13091996@gmail.com>
2022-09-07 02:14:03 -0300

    Memory access + two inputs

--- main.js
+++ main.js
@@ -1,30 +1,108 @@
-const originalProfileField = document.getElementById('thefile');
+const originalProfileField = document.getElementById('firstFile');
+const newProfileField = document.getElementById('secondFile');
+const form = document.getElementById('form');
+localStorage.clear();
 
-function parsingXml(xmlDoc)
+function filterJsonTrash(json)
 {
-    var title = xmlDoc.getElementsByTagName("fullName");
-    console.log(title);
-    console.log('fullName: '+ title[0].childNodes[0].textContent);
-    console.log('title.length: '+ title.length);
-
-    for(var i = 0; i < title.length; i++)
+    if (json.CustomField["#text"]) 
     {
-        console.log('Node title: '+title[i].childNodes[0].textContent);
+        delete json.CustomField["#text"];
     }
+    return json;
+}
+
+function xmlToJson(xml) 
+{
+	// Create the return object
+	var obj = {};
+
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+                console.log(attribute);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+};
+
+form.onsubmit = function(event){
+    if (localStorage.getItem('inputProfile') && localStorage.getItem('secondInputProfile'))
+    {
+        console.log('Diff the fields!');
+    }
+    else { console.log('Its missing a file...');}
+    return false;
+}
+
+function checkVar(element)
+{
+    console.log('CheckVar: inputProfile: ' +localStorage.getItem('inputProfile'));
+    console.log('CheckVar: secondInputProfile: ' +localStorage.getItem('secondInputProfile'));
 }
 
 originalProfileField.addEventListener('change', (event) => {
     const OriginalProfile = event.target.files[0];
     const reader = new FileReader();
-    console.log('Name:'+ OriginalProfile.name);
-    console.log('Size:'+ OriginalProfile.size);
-    console.log('Type:'+ OriginalProfile.type);
-    reader.readAsText(OriginalProfile)
+    reader.readAsText(OriginalProfile);
     reader.onload = function() {
-        console.log('Blob:'+ reader.result);
         var parser = new DOMParser();
         var xmlDoc = parser.parseFromString(reader.result,"text/xml");
-        parsingXml(xmlDoc);
+        var inputProfile = xmlToJson(xmlDoc);
+        inputProfile = filterJsonTrash(inputProfile);
+        console.log('inputProfile: '+JSON.stringify(inputProfile));
+        localStorage.setItem('inputProfile',inputProfile);
+    }
+    
+})
+
+newProfileField.addEventListener('change', (event) => {
+    const newProfile = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsText(newProfile)
+    reader.onload = function() {
+        var parser = new DOMParser();
+        var xmlDoc = parser.parseFromString(reader.result,"text/xml");
+        var secondInputProfile = xmlToJson(xmlDoc);
+        secondInputProfile = filterJsonTrash(secondInputProfile);
+        console.log('secondInputProfile: '+JSON.stringify(secondInputProfile));
+        localStorage.setItem('secondInputProfile',secondInputProfile);
     }
 })
+
+function diffJsons(jsonOld,jsonNew)
+{
+    console.log('Entered diffJsons');
+    //For each node of the NEW, look at the old
+    // then for each node of the old, search at the new
+
+    //Show them to the user
+}
+
+
 
